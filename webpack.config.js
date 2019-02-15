98a151280fe81fa45f84d18da324dd7962a426b6
dimezilla <diamond.joshh@gmail.com>
2019-02-15 12:52:42 -0500

    moving src to assets

--- webpack.config.js
+++ webpack.config.js
@@ -4,39 +4,42 @@ const CleanWebPackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: {
-    'bootstrap-block-editor': [
-      './src/scripts/bootstrap-block-editor.js',
-      './src/styles/bootstrap-block-editor.scss'
-    ]
-  },
-  output: {
-    filename: 'bootstrap-block-editor-[hash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebPackPlugin(['dist']),
-    new ManifestPlugin(),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: '[name].[hash].css',
-      chunkFilename: '[id].[hash].css',
-    })
-  ]
+	entry: {
+		'bootstrap-block-editor': [
+			'./assets/scripts/bootstrap-block-editor.js',
+			'./assets/styles/bootstrap-block-editor.scss'
+		],
+		'bootstrap-styles': [
+			'./assets/styles/bootstrap.scss'
+		]
+	},
+	output: {
+		filename: 'bootstrap-block-editor-[hash].js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	 module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			{
+				test: /\.scss$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+			}
+		]
+	},
+	plugins: [
+		new CleanWebPackPlugin(['dist']),
+		new ManifestPlugin(),
+		new MiniCssExtractPlugin({
+			// Options similar to the same options in webpackOptions.output
+			// both options are optional
+			filename: '[name].[hash].css',
+			chunkFilename: '[id].[hash].css',
+		})
+	]
 };
